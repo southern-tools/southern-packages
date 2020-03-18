1c94ea9ccb4c7a6143b7292f1ea58f8e181d9e6d
Diego <diego.samuelle@gmail.com>
2020-03-18 12:10:36 +0100

    Automatic Update

--- sys-config/user-config/files_common/user_config/scripts/mpd.sh
+++ sys-config/user-config/files_common/user_config/scripts/mpd.sh
@@ -4,4 +4,4 @@
 set -x
 
 mpd ~/.user_config/applications/mpd/mpd.conf
-i3-msg 'exec --no-startup-id urxvt -e ncmpcpp -c ~/.user_config/applications/ncmpcpp/config && killall mpd && killall ncmpcpp'
+swaymsg 'exec urxvt -e ncmpcpp -c ~/.user_config/applications/ncmpcpp/config && killall mpd && killall ncmpcpp'
