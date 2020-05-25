0e1974ae58711a6dce689c51bddb749c2dabb33d
Diego <diego.samuelle@gmail.com>
2020-05-25 14:14:50 +0200

    Automatic Update

--- sys-config/southern-config/files_skel/home/USER/user_config/scripts/mpd.sh
+++ sys-config/southern-config/files_skel/home/USER/user_config/scripts/mpd.sh
@@ -4,4 +4,4 @@
 #set -x
 
 mpd ~/.user_config/applications/mpd/mpd.conf
-swaymsg 'exec alacritty -e ncmpcpp -c ~/.user_config/applications/ncmpcpp/config && killall mpd && killall ncmpcpp'
+swaymsg 'exec urxvt -e ncmpcpp -c ~/.user_config/applications/ncmpcpp/config && killall mpd && killall ncmpcpp'
