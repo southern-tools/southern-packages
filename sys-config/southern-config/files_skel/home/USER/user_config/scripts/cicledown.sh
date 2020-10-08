b040c7a61866c2aceb8af35d0706a8d91c6c08c4
Diego <diego.samuelle@gmail.com>
2020-10-08 21:39:10 +0200

    Automatic Update

--- sys-config/southern-config/files_skel/home/USER/user_config/scripts/cicledown.sh
+++ sys-config/southern-config/files_skel/home/USER/user_config/scripts/cicledown.sh
@@ -3,10 +3,4 @@
 #
 #set -x
 
-swaymsg 'exec urxvt -e ~/.user_config/no_share/overlay_update.sh && \
-vdirsyncer sync user_contacts && \
-vdirsyncer sync user_calendar && \
-sudo genup && \
-sudo /etc/cron.daily/rsnapshot.daily && \
-pkill -HUP X && \
-sudo /sbin/halt'
+swaymsg 'exec urxvt -e ~/.user_config/no_share/overlay_update.sh && vdirsyncer sync user_contacts && vdirsyncer sync user_calendar && sudo genup && sudo /etc/cron.daily/rsnapshot.daily && pkill -HUP X && sudo /sbin/halt'
