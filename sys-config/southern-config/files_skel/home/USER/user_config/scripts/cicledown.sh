4ebbb33d1b2d185682552087bdd082abe0c4c7e9
Diego <diego.samuelle@gmail.com>
2020-10-08 22:06:45 +0200

    Automatic Update

--- sys-config/southern-config/files_skel/home/USER/user_config/scripts/cicledown.sh
+++ sys-config/southern-config/files_skel/home/USER/user_config/scripts/cicledown.sh
@@ -6,6 +6,6 @@
 ~/.user_config/no_share/overlay_update.sh ;
 vdirsyncer sync user_contacts ;
 vdirsyncer sync user_calendar ;
-sudo genup ;
+sudo genup -n -p;
 sudo /etc/cron.daily/rsnapshot.daily ;
 pkill -HUP X && sudo /sbin/halt
