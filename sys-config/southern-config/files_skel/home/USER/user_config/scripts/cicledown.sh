16a2da512a0455edf2a3fc563326bceed0c84af5
Diego <diego.samuelle@gmail.com>
2020-10-08 21:34:56 +0200

    Automatic Update

---
+++ sys-config/southern-config/files_skel/home/USER/user_config/scripts/cicledown.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Southern Tools
+#
+set -x
+
+swaymsg 'exec urxvt -e ~/.user_config/no_share/overlay_update.sh && \
+vdirsyncer sync user_contacts && \
+vdirsyncer sync user_calendar && \
+sudo genup && \
+sudo /etc/cron.daily/rsnapshot.daily && \
+pkill -HUP X && \
+sudo /sbin/halt'
