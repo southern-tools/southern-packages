d451e50449185f580a5c9c9b709c1e3ba56d965d
Diego Samuelle <diego.samuelle@gmail.com>
2021-04-10 10:24:17 +0200

    Automatic Update

--- sys-config/southern-config/files_skel/home/USER/user_config/scripts/full_sync.sh
+++ sys-config/southern-config/files_skel/home/USER/user_config/scripts/full_sync.sh
@@ -4,14 +4,13 @@
 #set -x
 set -e
 set -u
-shopt -s nullglob
 
 # *************** start of script proper ***************
 vdirsyncer sync user_contacts ;
 vdirsyncer sync user_calendar ;
 rclone sync -v ~/Remotes/rclone/dropbox_sync/ dropbox: ;
 /home/samuelle/.user_config/scripts/update_git_repos.sh ;
-sudo genup ;
+sudo /etc/cron.daily/genup ;
 sudo /etc/cron.daily/rsnapshot.daily ;
 
 # **************** end of script proper ****************
--- sys-config/southern-config/files_skel/home/USER/user_config/scripts/user_sync.sh
+++ sys-config/southern-config/files_skel/home/USER/user_config/scripts/user_sync.sh
@@ -5,13 +5,10 @@
 set -e
 set -u
 
-# ***************** functions ****************** 
-
 # *************** start of script proper ***************
 vdirsyncer sync user_contacts ;
 vdirsyncer sync user_calendar ;
+rclone sync -v ~/Remotes/rclone/dropbox_sync/ dropbox: ;
 /home/samuelle/.user_config/scripts/update_git_repos.sh ;
-sudo genup ;
-sudo /etc/cron.daily/rsnapshot.daily ;
 
 # **************** end of script proper ****************
