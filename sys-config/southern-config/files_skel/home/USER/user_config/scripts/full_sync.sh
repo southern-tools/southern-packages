38820504315bdc30debf4ba7cb27f46164172c94
Diego Samuelle <diego.samuelle@gmail.com>
2021-04-12 02:00:08 +0200

    Automatic Update

--- sys-config/southern-config/files_skel/home/USER/user_config/scripts/full_sync.sh
+++ sys-config/southern-config/files_skel/home/USER/user_config/scripts/full_sync.sh
@@ -8,8 +8,8 @@ set -u
 # *************** start of script proper ***************
 echo -e "*** Starting Full System Sync..."
 /home/samuelle/.user_config/scripts/user_sync.sh ;
-echo -e "*** Updating and Ugrading the system..."
-sudo genup ;
+#echo -e "*** Updating and Ugrading the system..."
+#sudo genup ;
 echo -e "*** Updating the daily backups..."
 sudo /etc/cron.daily/rsnapshot.daily ;
 echo -e "*** All tasks accomplished."
